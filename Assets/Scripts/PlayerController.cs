695029b5e084b3d1afbaf87a56e351d2e03373b2
dayzline <69827441+dayzline@users.noreply.github.com>
2021-05-07 11:21:26 -0700

    Finished Lesson C

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,6 +9,9 @@ public class PlayerController : MonoBehaviour
     Renderer pRenderer;
 
     public float speed;
+    bool hasPowerUp = false;
+    public float powerUpSpeed;
+    public GameObject powerUpInd;
 
     // Start is called before the first frame update
     void Start()
@@ -31,5 +34,35 @@ public class PlayerController : MonoBehaviour
         else
             pRenderer.material.color = new Color(1.0f + forwardInput, 1, 1.0f + forwardInput);
 
+        powerUpInd.transform.position = transform.position;
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("PowerUp"))
+        {
+            hasPowerUp = true;
+            Destroy(other.gameObject);
+            powerUpInd.SetActive(true);
+            StartCoroutine(PowerUpCountdown());            
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (hasPowerUp && collision.gameObject.CompareTag("Enemy"))
+        {
+            Rigidbody rbEnemy = collision.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayDir = collision.gameObject.transform.position - transform.position;
+
+            rbEnemy.AddForce(awayDir * powerUpSpeed, ForceMode.Impulse);
+        }
+    }
+    IEnumerator PowerUpCountdown()
+    {
+        yield return new WaitForSeconds(8);
+        hasPowerUp = false;
+        powerUpInd.SetActive(false);
     }
 }
